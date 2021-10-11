cedb7e565592c20ea4eec3effbc8b0d0c9f82939
Victor <vbianconi@afsgroup.nl>
2021-10-11 10:09:32 +0200

    Add expense component creation. Pushing to master before creating new branch because forgot

---
+++ src/components/Overview/AddExpense.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="add-expense">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class AddExpense extends Vue {
+  // Props go here
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ .add-expense {
+   width: 90%;
+   display: flex;
+   align-items: center;
+   margin: 1rem 0rem;
+ }
+</style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="overview">
-    <PageTitle
+    <page-title
       title="Overview"
       subtitle="Budgeteller tells you how much money you spend and how you can save more money."
     />
-    <OverviewStats stats="stats" :todays-date="todaysDate" />
+    <add-expense/>
+    <overview-stats stats="stats" :todays-date="todaysDate" />
   </div>
 </template>
 
@@ -13,9 +14,11 @@ import { Component, Vue } from "vue-property-decorator";
 import PageTitle from "@/components/PageTitle.vue"; // @ is an alias to /src
 import OverviewStats from "@/components/Overview/OverviewStats.vue";
 import dayjs from "dayjs";
+import AddExpense from "@/components/Overview/AddExpense.vue";
 
 @Component({
   components: {
+    AddExpense,
     PageTitle,
     OverviewStats,
   },
